12e202473be59c964bac5108df45510e14ee4adc
hitgub33 <135486923+hitgub33@users.noreply.github.com>
2024-01-01 10:36:16 +0800

    Update countdown.vue

--- docs/examples/statistic/countdown.vue
+++ docs/examples/statistic/countdown.vue
@@ -9,23 +9,7 @@
         format="HH:mm:ss"
         :value="value1"
       />
-      <el-button class="countdown-footer" type="primary" @click="reset"
-        >Reset
-      </el-button>
-    </el-col>
-    <el-col :span="8">
-      <el-countdown format="DD [days] HH:mm:ss" :value="value2">
-        <template #title>
-          <div style="display: inline-flex; align-items: center">
-            <el-icon style="margin-right: 4px" :size="12">
-              <Calendar />
-            </el-icon>
-            Still to go until next month
-          </div>
-        </template>
-      </el-countdown>
-      <div class="countdown-footer">{{ value2.format('YYYY-MM-DD') }}</div>
-    </el-col>
+     
   </el-row>
 </template>
 
